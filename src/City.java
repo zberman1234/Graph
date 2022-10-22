0b3ab3cbaa78021914414481efb8bf1a1c505df1
zach berman <107023337+zberman1234@users.noreply.github.com>
2022-10-22 11:02:31 +0000

    t

--- src/City.java
+++ src/City.java
@@ -1,6 +1,6 @@
 public class City {
   private String moniker;
-  private int last; // score in last football game
+  private int last; // score in their last football game
 
   public City(String mon, int lastsc) {
     moniker = mon;
